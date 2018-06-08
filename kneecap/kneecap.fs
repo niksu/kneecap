b9b78ca4a6ba5f47d0c6069ace269c18b642f186
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-06-07 21:07:04 -0400

    packet generators are now sensitive to packet mutators;

--- kneecap/kneecap.fs
+++ kneecap/kneecap.fs
@@ -25,23 +25,23 @@ breaking up deterministic computations, backtracking, and "learning" non-viable 
 controlling the backend solver
 *)
 
-let generate_packets (generator : packets.packet) (quantity : uint32) : (uint32 * byte[]) list =
+let generate_packets (generator : packets.packet) (quantity : uint32) (f : packets.packet -> bool) : (uint32 * byte[]) list =
   general.enumerate (int quantity - 1)
   |> List.map
       (fun i ->
          if not (generator.generate ()) then
            failwith "Failed to generate"
          let result = generator.extract_packet ()
-         if not (generator.constrain_different ()) then
-           failwith "Failed to constraint different"
+         if not (f generator) then
+           failwith "Failed to apply f constraint"
          match result with
          | None -> failwith "Could not generate a packet"
          | Some bytes -> (uint32 i, bytes))
 
 (*NOTE i assume that the generator has already been constrained*)
-let generate_pcap_contents (generator : packets.packet) (quantity : uint32) : pcap.pcap_file_contents =
+let generate_pcap_contents (generator : packets.packet) (quantity : uint32) (f : packets.packet -> bool) : pcap.pcap_file_contents =
 (*FIXME give raw packets as a parameter to generator, together with metadata (per-packet pcap header)*)
-  let raw_packets = generate_packets generator quantity
+  let raw_packets = generate_packets generator quantity f
   assert (List.length raw_packets = int quantity)
   assert (generator.packet_size <= pcap.default_global_header.snaplen)
   {global_header = pcap.default_global_header;
@@ -57,7 +57,7 @@ let generate_pcap_contents (generator : packets.packet) (quantity : uint32) : pc
     }
 
 
-let generate_timed_pcap_contents (generator : packets.packet) (quantity : uint32) (f : packets.packet -> unit) : pcap.pcap_file_contents =
+let generate_timed_pcap_contents (generator : packets.packet) (quantity : uint32) (f : packets.packet -> bool) : pcap.pcap_file_contents =
   assert (generator.packet_size <= pcap.default_global_header.snaplen)
 
   use countdown = System.Timers.Timer(30000.)
@@ -75,8 +75,8 @@ let generate_timed_pcap_contents (generator : packets.packet) (quantity : uint32
          if not (generator.generate ()) then
            failwith "Failed to generate"
          let result = generator.extract_packet ()
-         if not (generator.constrain_different ()) then
-           failwith "Failed to constraint different"
+         if not (f generator) then
+           failwith "Failed to apply f constraint"
          match result with
          | None -> failwith "Could not generate a packet"
          | Some bytes -> bytes
